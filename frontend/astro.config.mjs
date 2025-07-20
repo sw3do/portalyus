cda6e5c8b6650ac900e0c794accf6ad18addede2
Mert <89777525+sw3do@users.noreply.github.com>
2025-07-20 22:54:52 +0300

    fix: static to server with nodejs adapter

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'static',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   server: {
     port: 3000,
   },
