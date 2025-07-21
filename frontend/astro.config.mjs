f2e44ca05bd21491cc73342f69e7499df0b936f6
Mert <89777525+sw3do@users.noreply.github.com>
2025-07-21 14:05:51 +0300

    fix: added vercel support

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,15 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel({}),
   server: {
     port: 3000,
   },
